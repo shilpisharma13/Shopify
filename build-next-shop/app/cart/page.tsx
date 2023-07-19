47f3f31007809f029d17040d52d88744acb50624
shilpisharma13 <shilpi13@gmail.com>
2023-07-19 13:12:19 +1000

    cart page css in progress

--- build-next-shop/app/cart/page.tsx
+++ build-next-shop/app/cart/page.tsx
@@ -1,224 +1,113 @@
+'use client'
+
 import Image from 'next/image'
+import { formatter } from '../../utils/helpers'
+import {
+  TrashIcon,
+  PlusSmallIcon,
+  MinusSmallIcon,
+} from '@heroicons/react/24/outline'
+import { useShopContext } from '@/context/shopContext'
 
 const CartPage = () => {
+  const { cart, cartLoading, checkoutUrl } = useShopContext()
   return (
-    <div className='bg-white'>
-      <main className='mx-auto mt-10'>
-        <div className='flex shadow-md my-10'>
-          <div className='w-3/4 bg-white px-10 py-10'>
-            <div className='flex justify-between border-b pb-8'>
-              <h1 className='font-semibold text-2xl'>Shopping Cart</h1>
-              <h2 className='font-semibold text-2xl'>3 Items</h2>
-            </div>
-            <div className='flex mt-10 mb-5'>
-              <h3 className='font-semibold text-gray-600 text-xs uppercase w-2/5'>
-                Product Details
-              </h3>
-              <h3 className='font-semibold text-center text-gray-600 text-xs uppercase w-1/5'>
-                Quantity
-              </h3>
-              <h3 className='font-semibold text-center text-gray-600 text-xs uppercase w-1/5'>
-                Price
-              </h3>
-              <h3 className='font-semibold text-center text-gray-600 text-xs uppercase w-1/5'>
-                Total
-              </h3>
-            </div>
-            <div className='flex items-center hover:bg-gray-100 -mx-8 px-6 py-5'>
-              <div className='flex w-2/5'>
-                <div className='w-20'>
-                  <Image className='h-24' src='' alt='' />
-                </div>
-                <div className='flex flex-col justify-between ml-4 flex-grow'>
-                  <span className='font-bold text-sm'>Iphone 6S</span>
-                  <span className='text-red-500 text-xs'>Apple</span>
-                  <a
-                    href='#'
-                    className='font-semibold hover:text-red-500 text-gray-500 text-xs'
-                  >
-                    Remove
-                  </a>
-                </div>
-              </div>
-              <div className='flex justify-center w-1/5'>
-                <svg
-                  className='fill-current text-gray-600 w-3'
-                  viewBox='0 0 448 512'
-                >
-                  <path d='M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z' />
-                </svg>
-
-                <input
-                  className='mx-2 border text-center w-8'
-                  type='text'
-                  value='1'
-                />
-
-                <svg
-                  className='fill-current text-gray-600 w-3'
-                  viewBox='0 0 448 512'
-                >
-                  <path d='M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z' />
-                </svg>
-              </div>
-              <span className='text-center w-1/5 font-semibold text-sm'>
-                $400.00
-              </span>
-              <span className='text-center w-1/5 font-semibold text-sm'>
-                $400.00
-              </span>
-            </div>
-
-            <div className='flex items-center hover:bg-gray-100 -mx-8 px-6 py-5'>
-              <div className='flex w-2/5'>
-                <div className='w-20'>
-                  <Image className='h-24' src='' alt='' />
-                </div>
-                <div className='flex flex-col justify-between ml-4 flex-grow'>
-                  <span className='font-bold text-sm'>Xiaomi Mi 20000mAh</span>
-                  <span className='text-red-500 text-xs'>Xiaomi</span>
-                  <a
-                    href='#'
-                    className='font-semibold hover:text-red-500 text-gray-500 text-xs'
-                  >
-                    Remove
-                  </a>
-                </div>
-              </div>
-              <div className='flex justify-center w-1/5'>
-                <svg
-                  className='fill-current text-gray-600 w-3'
-                  viewBox='0 0 448 512'
-                >
-                  <path d='M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z' />
-                </svg>
-
-                <input
-                  className='mx-2 border text-center w-8'
-                  type='text'
-                  value='1'
-                />
-
-                <svg
-                  className='fill-current text-gray-600 w-3'
-                  viewBox='0 0 448 512'
-                >
-                  <path d='M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z' />
-                </svg>
-              </div>
-              <span className='text-center w-1/5 font-semibold text-sm'>
-                $40.00
-              </span>
-              <span className='text-center w-1/5 font-semibold text-sm'>
-                $40.00
-              </span>
-            </div>
-
-            <div className='flex items-center hover:bg-gray-100 -mx-8 px-6 py-5'>
-              <div className='flex w-2/5'>
-                {' '}
-                /
-                <div className='w-20'>
-                  <Image className='h-24' src='' alt='' />
-                </div>
-                <div className='flex flex-col justify-between ml-4 flex-grow'>
-                  <span className='font-bold text-sm'>Airpods</span>
-                  <span className='text-red-500 text-xs'>Apple</span>
-                  <a
-                    href='#'
-                    className='font-semibold hover:text-red-500 text-gray-500 text-xs'
-                  >
-                    Remove
-                  </a>
-                </div>
-              </div>
-              <div className='flex justify-center w-1/5'>
-                <svg
-                  className='fill-current text-gray-600 w-3'
-                  viewBox='0 0 448 512'
-                >
-                  <path d='M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z' />
-                </svg>
-                <input
-                  className='mx-2 border text-center w-8'
-                  type='text'
-                  value='1'
-                />
-
-                <svg
-                  className='fill-current text-gray-600 w-3'
-                  viewBox='0 0 448 512'
-                >
-                  <path d='M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z' />
-                </svg>
-              </div>
-              <span className='text-center w-1/5 font-semibold text-sm'>
-                $150.00
-              </span>
-              <span className='text-center w-1/5 font-semibold text-sm'>
-                $150.00
-              </span>
-            </div>
-
-            <a
-              href='/products'
-              className='flex font-semibold text-indigo-600 text-sm mt-10'
+    <div className='bg-white flex px-4 flex-grow justify-between mt-10'>
+      <div className='mx-2 px-5 py-5 basis-3/4'>
+        <div className='flex justify-between border-b pb-2'>
+          <h1 className='font-semibold text-2xl'>Your Cart</h1>
+          <h2 className='text-gray-600 text-m'>3 Items in cart</h2>
+        </div>
+        {cart.map((product) => {
+          return (
+            <div
+              key={product.id}
+              className='flex justify-between hover:bg-gray-100  px-3 py-5'
             >
-              <svg
-                className='fill-current mr-2 text-indigo-600 w-4'
-                viewBox='0 0 448 512'
-              >
-                <path d='M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z' />
-              </svg>
+              <div>
+                <div className='flex grow'>
+                  <div className='relative flex-shrink-0 w-24 h-20 overflow-hidden border border-gray-200 rounded-md'>
+                    <Image
+                      src={product.image}
+                      alt={product.title}
+                      layout='fill'
+                      objectFit='cover'
+                    />
+                  </div>
+                  <div className='flex flex-col  ml-4 flex-grow'>
+                    <span className='font-bold text-sm'>{product.title}</span>
+                    <span className='text-gray-500 text-m'>
+                      {product.variantTitle}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className='grid grid-cols-3'>
+                <div>
+                  <button
+                    className='px-2'
+                    onClick={() => decrementCartItem(product)}
+                    disabled={cartLoading}
+                  >
+                    -
+                  </button>
+                </div>
+                <div>
+                  <span className='px-2 border-l border-r'>
+                    {product.variantQuantity}
+                  </span>
+                </div>
+                <div>
+                  <button
+                    className='px-2'
+                    onClick={() => incrementCartItem(product)}
+                    disabled={cartLoading}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <div>{formatter.format(product.variantPrice)}</div>
+              <div>
+                <TrashIcon className='ml-3 w-5' />
+              </div>
+            </div>
+          )
+        })}
+      </div>
+
+      <div
+        id='summary'
+        className='mx-3 px-8 py-8 border rounded-lg bg-gray-600'
+      >
+        <h1 className='font-semibold text-2xl text-white'>Order Summary</h1>
+        <div className='flex justify-between mt-10 mb-2 text-white'>
+          <span className='font-semibold text-sm uppercase'>Total</span>
+          <span className='font-semibold text-sm'>590$</span>
+        </div>
+        <div className='flex justify-between mt-5 mb-2 text-white'>
+          <span className='font-semibold text-sm uppercase'>Shipping</span>
+          <span className='font-semibold text-sm'>$10</span>
+        </div>
+
+        <div className='border-t mt-4'>
+          <div className='flex font-semibold justify-between py-6 text-sm uppercase text-white'>
+            <span>Total cost</span>
+            <span>$600</span>
+          </div>
+          <div className='grid grid-row2-2'>
+            <button className=' items-center justify-center px-6 py-3 text-base font-medium text-white bg-black border border-transparent rounded-md shadow-sm hover:bg-gray-800 mb-2'>
+              Checkout
+            </button>
+            <a
+              className=' items-center justify-center px-6 py-3 text-base font-medium text-black bg-white border  rounded-md shadow-sm '
+              href='/products'
+            >
               Continue Shopping
             </a>
           </div>
-
-          <div id='summary' className='w-1/4 px-8 py-10'>
-            <h1 className='font-semibold text-2xl border-b pb-8'>
-              Order Summary
-            </h1>
-            <div className='flex justify-between mt-10 mb-5'>
-              <span className='font-semibold text-sm uppercase'>Items 3</span>
-              <span className='font-semibold text-sm'>590$</span>
-            </div>
-            <div>
-              <label className='font-medium inline-block mb-3 text-sm uppercase'>
-                Shipping
-              </label>
-              <select className='block p-2 text-gray-600 w-full text-sm'>
-                <option>Standard shipping - $10.00</option>
-              </select>
-            </div>
-            <div className='py-10'>
-              <label
-                id='promo'
-                className='font-semibold inline-block mb-3 text-sm uppercase'
-              >
-                Promo Code
-              </label>
-              <input
-                type='text'
-                id='promo'
-                placeholder='Enter your code'
-                className='p-2 text-sm w-full'
-              />
-            </div>
-            <button className='bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase'>
-              Apply
-            </button>
-            <div className='border-t mt-8'>
-              <div className='flex font-semibold justify-between py-6 text-sm uppercase'>
-                <span>Total cost</span>
-                <span>$600</span>
-              </div>
-              <button className='flex items-center justify-center px-6 py-3 text-base font-medium text-white bg-black border border-transparent rounded-md shadow-sm hover:bg-gray-800'>
-                Checkout
-              </button>
-            </div>
-          </div>
         </div>
-      </main>
+      </div>
     </div>
   )
 }
